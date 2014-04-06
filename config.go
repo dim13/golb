f5d74048644c4004da66fe86f56f216eb0c855eb
Dimitri Sokolyuk <demon@dim13.org>
2014-04-06 03:50:34 +0200

    split config

--- config.go
+++ config.go
@@ -4,28 +4,48 @@ import (
 	"code.google.com/p/gcfg"
 )
 
-type Config struct {
-	Main
+type settings struct {
+	ArticlesPerPage int
+	TagsInCloud int
+	Db string
 }
 
-type Main struct {
-	DataBase          string
-	BlogTheme         string
-	BlogTitle         string
-	BlogSubtitle      string
-	BlogUrl           string
-	BlogOwner         string
-	BlogRights        string
-	CaptchaPublic     string
-	CaptchaPrivate    string
-	CommentLength     int
-	CommentsAllowed   bool
-	SmtpServer        string
-	SmtpSender        string
-	ArticlesPerPage   int
-	GoogleAnalyticsId string
-	GoogleWebmasterId string
-	TagsInCloud       int
+type blog struct {
+	Title string
+	Subtitle string
+	Url string
+	Theme string
+	Owner string
+	Rights string
+}
+
+type captcha struct {
+	Public string
+	Private string
+}
+
+type comments struct {
+	Maxlen int
+	Allowed bool
+}
+
+type smtp struct {
+	Server string
+	Sender string
+}
+
+type google struct {
+	AnalyticsID string
+	WebmasterID string
+}
+
+type Config struct {
+	Settings settings
+	Blog blog
+	Captcha captcha
+	Comments comments
+	Smtp smtp
+	Google google
 }
 
 func ReadConf(fname string) (*Config, error) {
