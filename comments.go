e9f401f649781957ad2bfb878d22ca05fff9cbec
Dimitri Sokolyuk <demon@dim13.org>
2014-04-28 17:14:14 +0200

    gofmt

--- comments.go
+++ comments.go
@@ -23,17 +23,9 @@ func (c *Comment) Suppress() {
 	c.Enabled = false
 }
 
-func (c Comments) Len() int {
-	return len(c)
-}
-
-func (c Comments) Swap(i, j int) {
-	c[i], c[j] = c[j], c[i]
-}
-
-func (c Comments) Less(i, j int) bool {
-	return c[i].Date.Before(c[j].Date)
-}
+func (c Comments) Len() int           { return len(c) }
+func (c Comments) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
+func (c Comments) Less(i, j int) bool { return c[i].Date.Before(c[j].Date) }
 
 func (c *Comments) Add(comment *Comment) {
 	comment.Date = time.Now()
@@ -41,7 +33,7 @@ func (c *Comments) Add(comment *Comment) {
 }
 
 func (c *Comment) PostDate() string {
-	return c.Date.Local().Format(TimeFormat)	// defined in articles.go
+	return c.Date.Local().Format(TimeFormat) // defined in articles.go
 }
 
 func (c Comments) Enabled() Comments {
