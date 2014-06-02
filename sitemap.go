16004c23aa89666466f0e8e79d888de63762e220
Dimitri Sokolyuk <demon@dim13.org>
2014-06-02 02:00:20 +0200

    add lastmod to all items

--- sitemap.go
+++ sitemap.go
@@ -23,12 +23,14 @@ func (sm Sitemap) LastMod() string {
 
 func sitemapHandler(w http.ResponseWriter, r *http.Request) {
 	var sm SiteMap
+	articles := data.Articles.Enabled()
 	sm = append(sm, Sitemap{
 		Loc:        "http://" + r.Host,
-		ChangeFreq: "daily",
 		Priority:   1.0,
+		Date:       articles[0].Date,
+		ChangeFreq: "daily",
 	})
-	for _, a := range data.Articles.Enabled() {
+	for _, a := range articles {
 		sm = append(sm, Sitemap{
 			Loc:        "http://" + r.Host + "/" + a.Slug,
 			Priority:   0.8,
@@ -37,9 +39,11 @@ func sitemapHandler(w http.ResponseWriter, r *http.Request) {
 		})
 	}
 	for _, t := range data.Articles.TagCloud() {
+		tagged := articles.Tag(t.Tag)
 		sm = append(sm, Sitemap{
 			Loc:        "http://" + r.Host + "/tag/" + t.Tag,
 			Priority:   0.6 - float64(t.Wight)/10,
+			Date:       tagged[0].Date,
 			ChangeFreq: "weekly",
 		})
 	}
