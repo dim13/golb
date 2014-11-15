da2ca72956fd217aab96a4697bf6cbb2153a60e4
Dimitri Sokolyuk <demon@dim13.org>
2014-11-15 01:36:03 +0100

    Follow lint suggestions

--- sitemap.go
+++ sitemap.go
@@ -6,44 +6,43 @@ import (
 	"time"
 )
 
-type SiteMap []Sitemap
-type Sitemap struct {
+type sitemap struct {
 	Loc        string
-	Date       time.Time
+	LastMod    string
 	ChangeFreq string
 	Priority   float64
 }
 
-func (sm Sitemap) LastMod() string {
-	if sm.Date.IsZero() {
+func lastMod(date time.Time) string {
+	if date.IsZero() {
 		return ""
 	}
-	return sm.Date.Local().Format("2006-02-01")
+	return date.Local().Format("2006-02-01")
 }
 
 func sitemapHandler(w http.ResponseWriter, r *http.Request) {
-	var sm []Sitemap
+	var sm []sitemap
 	articles := art.Enabled()
-	sm = append(sm, Sitemap{
+	sm = append(sm, sitemap{
 		Loc:        "http://" + r.Host,
 		Priority:   1.0,
-		Date:       articles[0].Date,
+		LastMod:    lastMod(articles[0].Date),
 		ChangeFreq: "daily",
 	})
 	for _, a := range articles {
-		sm = append(sm, Sitemap{
+		sm = append(sm, sitemap{
 			Loc:        "http://" + r.Host + "/" + a.Slug,
 			Priority:   0.8,
-			Date:       a.Date,
+			LastMod:    lastMod(a.Date),
 			ChangeFreq: "monthly",
 		})
 	}
 	for _, t := range art.TagCloud() {
 		tagged := articles.Tag(t.Tag)
-		sm = append(sm, Sitemap{
+		sm = append(sm, sitemap{
 			Loc:        "http://" + r.Host + "/tag/" + t.Tag,
 			Priority:   0.6 - float64(t.Wight)/10,
-			Date:       tagged[0].Date,
+			LastMod:    lastMod(tagged[0].Date),
 			ChangeFreq: "weekly",
 		})
 	}
