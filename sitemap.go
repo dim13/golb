9d67f6a02b71dc21b1f2fdbb8a488815e9c85cb4
Dimitri Sokolyuk <demon@dim13.org>
2014-11-22 03:06:18 +0100

    add some space, remove tmp logging

--- sitemap.go
+++ sitemap.go
@@ -22,24 +22,27 @@ func lastMod(date time.Time) string {
 
 func sitemapHandler(w http.ResponseWriter, r *http.Request) {
 	var sm []sitemap
-	log.Println(blog)
 	be := blog.Enabled()
+
 	sm = append(sm, sitemap{
 		Loc:      "http://" + r.Host,
 		Priority: 1.0,
 	})
+
 	for _, a := range be.Articles() {
 		sm = append(sm, sitemap{
 			Loc:      "http://" + r.Host + "/" + a.Slug(),
 			Priority: 0.8,
 		})
 	}
+
 	for t, a := range be.TagMap() {
 		sm = append(sm, sitemap{
 			Loc:      "http://" + r.Host + "/tag/" + t,
 			Priority: 0.6 - float64(5/len(a))/10,
 		})
 	}
+
 	err := tmpl.ExecuteTemplate(w, "sitemap.tmpl", sm)
 	if err != nil {
 		log.Println(err)
