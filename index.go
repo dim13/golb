e12fa11e2976b5f197a92a7cfbb041f4aff86ad2
Dimitri Sokolyuk <demon@dim13.org>
2015-01-19 01:20:03 +0100

    Split archive

--- index.go
+++ index.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"sort"
 	"strconv"
 	"time"
 
@@ -28,29 +27,6 @@ type page struct {
 	LastYear  int
 }
 
-type byYear []year
-type year struct {
-	Year  int
-	Count int
-	Month []month
-}
-
-type byMonth []month
-type month struct {
-	Month    time.Month
-	Year     int
-	Count    int
-	Articles blog.Articles
-}
-
-func (m byMonth) Len() int           { return len(m) }
-func (m byMonth) Swap(i, j int)      { m[i], m[j] = m[j], m[i] }
-func (m byMonth) Less(i, j int) bool { return m[i].Month < m[j].Month }
-
-func (y byYear) Len() int           { return len(y) }
-func (y byYear) Swap(i, j int)      { y[i], y[j] = y[j], y[i] }
-func (y byYear) Less(i, j int) bool { return y[i].Year < y[j].Year }
-
 func atoiMust(s string) int {
 	if s == "" {
 		return 0
@@ -61,37 +37,6 @@ func atoiMust(s string) int {
 		return 0
 	}
 	return i
-}
-
-func (p *page) MakeArchive() {
-	if p.Year == 0 {
-		p.Year = p.Articles.Head().Year()
-	}
-	if p.Month == 0 {
-		p.Month = p.Articles.Head().Month()
-	}
-	for y, v := range Blog.Articles().YearMap() {
-		year := year{
-			Year:  y,
-			Count: len(v),
-		}
-		if p.Year == y {
-			for m, v := range v.MonthMap() {
-				month := month{
-					Year:  y,
-					Month: time.Month(m),
-					Count: len(v),
-				}
-				if p.Month == time.Month(m) {
-					month.Articles = v
-				}
-				year.Month = append(year.Month, month)
-			}
-			sort.Sort(byMonth(year.Month))
-		}
-		p.Archive = append(p.Archive, year)
-	}
-	sort.Sort(sort.Reverse(byYear(p.Archive)))
 }
 
 func (p *page) Pager(pg string) {
--- sitemap.go
+++ sitemap.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-	"time"
 )
 
 type sitemap struct {
